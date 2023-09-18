bad8ea3f6ae5ea9c97ae8ed5d95de70bac9228e5
Zak Goneau <zak.goneau@stu.ocsb.ca>
2023-09-18 16:36:54 +0000

    Hello, World! With proper style.

---
+++ hello_world_style.py
@@ -0,0 +1,14 @@
+#!/user/bin/env python3
+
+# Created By: Zak Goneau
+# Date: Sep. 18, 2023
+# This program displays Hello, World! with proper style
+
+
+def main():
+    print("Hello, World! With proper style!")
+    print("I am testing my function here.")
+
+
+if __name__ == "__main__":
+    main()
